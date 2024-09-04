b99cc1590b96c5149edf3dcbde187463b42f2d87
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-09-04 21:46:48 +0200

    feat: export typography

--- src/components/ResetCss/index.ts
+++ src/components/ResetCss/index.ts
@@ -1,1 +1,1 @@
-export { default as Button } from './ResetCss.tsx';
+export { default as ResetCss } from './ResetCss.tsx';
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 export * from "./components/Button";
+export * from "./components/ResetCss";
+export * from "./components/Typography";
