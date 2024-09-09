6e2fec11af2d4393ce3abcc43190ce1f85bfb3b7
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-09-09 19:56:30 +0200

    feat: create image component

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,14 +1,23 @@
 import { FC } from "react";
 import IImage from "./Image.types";
+import "./image.styles.scss";
 
-const Image: FC<IImage> = ({ src, alt, width, height, className, testId }) => {
+const Image: FC<IImage> = ({
+  src,
+  alt,
+  width,
+  responsive,
+  height,
+  className,
+  testId,
+}) => {
   return (
     <img
       src={src}
       alt={alt}
       width={width}
       height={height}
-      className={className}
+      className={`${responsive && "responsive"} ${className}`}
       data-testid={testId}
     />
   );
