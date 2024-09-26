5c18e9e9ef2d60186bdadb906f9662cda5c93625
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-09-26 12:19:22 +0200

    chore: add variant to Header type

--- src/components/Header/Header.types.ts
+++ src/components/Header/Header.types.ts
@@ -1,8 +1,10 @@
 import { ReactNode } from "react";
 import IBasic from "../../types/IBasic";
+import TVariant from "../../types/TVariants";
 
 interface IHeader extends IBasic {
   children: ReactNode;
+  variant?: TVariant;
 }
 
 export default IHeader;
