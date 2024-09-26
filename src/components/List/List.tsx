3dd4db1027fbfa2fe19a57303a56e667982508f5
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-09-26 12:20:28 +0200

    feat: create List component

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,15 +1,45 @@
 import { FC } from "react";
-import IList, { IListItem } from "./List.types";
+import IList from "./List.types";
+import ITypography from "../Typography/Typography.types";
+import { Typography } from "../Typography";
+import "./list.styles.scss";
+import "../Flex/flex.styles.scss";
 
-const List: FC<IList> = ({ as: Component = "ul", children }) => {
-  return <Component>{children}</Component>;
+const List: FC<IList> & { item: FC<ITypography> } = ({
+  as: Component = "ul",
+  integration = "vertical",
+  spacing,
+  children,
+}) => {
+  return (
+    <Component className={`list list--${integration} list--${spacing}`}>
+      {children}
+    </Component>
+  );
 };
 
-const ListItem: FC<IListItem> = ({ children }) => {
-  if (children) {
-    
-  }
-  return <li>Item</li>;
-}
+const ListItem: FC<ITypography> = ({
+  as: Component = "p",
+  children,
+  variant = "dark",
+  size = "md",
+  weight = "normal",
+  ...props
+}) => {
+  return (
+    <Typography
+      as="li"
+      size={size}
+      variant={variant}
+      weight={weight}
+      className="list__item"
+      {...props}
+    >
+      {children}
+    </Typography>
+  );
+};
+
+List.item = ListItem;
 
 export default List;
