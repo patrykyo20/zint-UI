de9cb873582a487be3dbd6c86c85c1e39d5c01de
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-09-26 12:15:35 +0200

    feat: create Burger component

---
+++ src/components/Burger/Burger.tsx
@@ -0,0 +1,11 @@
+import "./burger.styles.scss";
+
+const Burger = () => (
+  <div className="burger">
+    <span className="burger__item"></span>
+    <span className="burger__item"></span>
+    <span className="burger__item"></span>
+  </div>
+);
+
+export default Burger;
