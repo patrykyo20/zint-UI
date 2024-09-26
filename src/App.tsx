260e4ea663fa874d9563a79db8a8d9c865b7f673
Patryk Wierzbowski <132001497+patrykyo20@users.noreply.github.com>
2024-09-26 12:12:50 +0200

    feat: Add cards and headers to app

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import Button from "./components/Button/Button";
 import ResetCss from "./components/ResetCss/ResetCss";
 import Input from "./components/Input/Input";
 import Textarea from "./components/Textarea/Textarea";
-import Image from "./components/Image/Image";
 import "./styles/main.scss";
 import Breadcrumbs from "./components/Breadcrumbs/Breadcrumbs";
-import Flex from "./components/Flex/Flex";
-import { Typography } from "./components/Typography";
-import { Card } from "./components/Card";
+import { Card1, Card2, Card3, Card4 } from "./testComponents/Cardtest";
+import Header1, { Header2, Header3, Header4 } from "./testComponents/Headertest";
 
 function App() {
   const [count, setCount] = useState<number>(0);
@@ -16,69 +14,16 @@ function App() {
   return (
     <>
       <ResetCss />
+
+      <Header1 />
+      <Header2 />
+      <Header3 />
+      <Header4 />
+      <Card1 />
+      <Card2 />
+      <Card3 />
+      <Card4 />
       <div>hello</div>
-      <Card variant="vertical">
-        <Image
-          src={"/office.jpeg"}
-          alt={"office-image"}
-          width={506}
-          height={256}
-          responsive
-        />
-        <Card.container>
-          <Typography size="3xl" weight="bold" as="h4">
-            Lorem Ipsum is simply Lorem Ipsum is simply
-          </Typography>
-          <Typography size="xl" as="p">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the Lorem Ipsum is simply dummy text
-            of the printing and typesetting industry. Lorem Ipsum has been the
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the{" "}
-          </Typography>
-          <Button>Read more</Button>
-        </Card.container>
-      </Card>
-      <Card variant="horizontal">
-        <Image
-          src={"/office.jpeg"}
-          alt={"office-image"}
-          width={506}
-          height={256}
-        />
-        <Card.container variant="horizontal">
-          <Typography size="3xl" weight="bold" as="h4">
-            Lorem Ipsum is simply Lorem Ipsum is simply
-          </Typography>
-          <Typography size="xl" as="p">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the Lorem Ipsum is simply dummy text
-            of the printing and typesetting industry. Lorem Ipsum has been the
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the{" "}
-          </Typography>
-          <Button>Read more</Button>
-        </Card.container>
-      </Card>
-      <Card variant="product">
-        <Image
-          src={"/iphoneProduct.png"}
-          alt={"iphone"}
-          width={211}
-          height={211}
-        />
-        <Card.container variant="product">
-          <Typography size="3xl" weight="bold" as="h4">
-            Lorem Ipsum is simply Lorem Ipsum is simply
-          </Typography>
-          <Flex justifyContent="space-between" alignItems="center">
-            <Typography size="4xl" as="p" weight="bold">
-              599$
-            </Typography>
-            <Button size="sm">Add to Cart</Button>
-          </Flex>
-        </Card.container>
-      </Card>
       <div>
         <Button>Hello</Button>
         <Button onClick={() => setCount(count + 1)} variant="primary" size="sm">
